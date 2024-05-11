844c603a4a27ea0f99bddc7389a5ae4a0e2341ed
norun9 <u.t012807@gmail.com>
2024-05-11 17:18:35 +0900

    exclude files at build generation from git control target

--- build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/Users/tomoyaueno/esp/esp-idf-v5.2.1/components/mbedtls/mbedtls/library/error.c
--- build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
+++
@@ -1,1 +0,0 @@
-/Users/tomoyaueno/esp/esp-idf-v5.2.1/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
